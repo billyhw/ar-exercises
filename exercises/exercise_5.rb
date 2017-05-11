f3fcc38a5d7b2bbab8d2724f7207cfdc333b7924
Billy Chang <billyheungwing@yahoo.ca>
2017-05-11 17:21:03 +0000

    completed ex 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,15 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+@total_revenue = Store.all.sum("annual_revenue")
+
+puts "Total Annual Revenue: #{@total_revenue}"
+
+@total_stores = Store.all.count
+
+# just in case the average is has decimals, turn the numerator and denominator to float
+puts "Average Revenue: #{@total_revenue.to_f/@total_stores.to_f}"
+
+@num_stores_over1M = Store.where("annual_revenue > 1000000").count
+
+puts "Number of stores with over $1M sales: #{@num_stores_over1M}"
