ccc0f943c099b2b40feafaffe559b25bc5c049a9
Billy Chang <billyheungwing@yahoo.ca>
2017-05-11 16:44:23 +0000

    completed ex 3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,11 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find(3)
+@store3.destroy
+
+# see if the database is updated
+puts "Number of stores: #{Store.all.count}"
+puts Store.find(1).name
+puts Store.find(2).name
+puts Store.find(3).name
