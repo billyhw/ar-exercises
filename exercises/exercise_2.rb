eba35a4875ca2b52e3ec744b310298feba6c4c83
Billy Chang <billyheungwing@yahoo.ca>
2017-05-11 16:36:25 +0000

    completed exercise 2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,17 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+@store1.name = "Toronto"
+@store1.annual_revenue = 3500000
+@store1.save
+
+puts "---- Check out @store1 ----"
+puts @store1.name
+puts @store1.annual_revenue
+
+puts "---- Check whether first entry in database is updated ----"
+puts Store.find(1).name
+puts Store.find(1).annual_revenue
